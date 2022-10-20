eb12092b8bf8543970fa7b9b838e8465a272a0ae
Gloria Paulina Gonzalez Hernandez <ghpaulina3@gmail.com>
2022-10-20 15:23:20 -0600

    commit tree fc5e1a9ad7ab30511afc1c1b5e1e3f6701365761

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+function multiply(num1, num2){
+return num1 * num2; 
+}
+console.log(multiply(31, 2));
 
-// Write your code here
+function random() {
+    return Math.floor(Math.random() * 100) + 1;
+   }
+   console.log(random());
+   
+   function mod(num3, num4){
+    return num3 / num4;
+  }
+  console.log(mod(20, 5));
+
+Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+  
